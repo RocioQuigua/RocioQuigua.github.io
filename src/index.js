a15d074752daaa445c25c663bf02174c67abb844
Rocio Quigua <rocioquigualopez@gmail.com>
2022-08-17 13:03:45 -0500

    feat(update): index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './routes/App';
-import './styles/global.scss';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
