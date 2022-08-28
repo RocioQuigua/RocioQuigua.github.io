6f1327d869cd61ec82a2795b3280e1cfa03a090d
Rocio Quigua <rocioquigualopez@gmail.com>
2022-08-27 23:23:36 -0500

    feat(containers): update

--- src/containers/ProductList/ProductList.jsx
+++ src/containers/ProductList/ProductList.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+
 import { ProductItem } from '../../components/ProductItem/ProductItem';
+import { useGetProducts } from '../../hooks/useGetProducts';
+
 import './ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 export const ProductList = () => {
+  const products = useGetProducts(API);
   return (
     <section className='mainContainer'>
       <div className='productList'>
-        <ProductItem />
+        {products.map((product) => (
+        <ProductItem product={product} key={product.id}/>
+        ))}
       </div>
     </section>
   );
