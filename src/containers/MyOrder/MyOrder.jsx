fa104cfc9c46f4bb7f9211ba1b7a80785bab83bd
Rocio Quigua <rocioquigualopez@gmail.com>
2022-08-29 21:46:40 -0500

    feat(containers): update

--- src/containers/MyOrder/MyOrder.jsx
+++ src/containers/MyOrder/MyOrder.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
 import { OrderItem } from '../../components/OrderItem/OrderItem';
+import { AppContext } from '../../context/AppContext';
+
 import './MyOrder.scss';
 
 export const MyOrder = () => {
+  const { state } = useContext(AppContext);
+
+  const sumTotal = () => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum;
+  }
+
   return (
     <aside className="myOrder">
     <div className="myOrder__container">
-      <img src="images/flechita.svg" alt="flechita" />
+      <img src="../../assets/images/flechita.svg" alt="flechita" />
       <p className="myOrder__title">My order</p>
     </div>
     <div className="myOrder__content">
-      <OrderItem />
+      {state.cart.map((product, index) => (
+      <OrderItem 
+      indexValue={index}
+      product={product} 
+      key={index}
+      />
+      ))}
       <div className="myOrder__orders">
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="myOrder__primary-button">
 					Checkout
